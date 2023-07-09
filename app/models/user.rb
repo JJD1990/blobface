755ef0d27788f5255ff74652508c6344f8e7d56c
JJD1990 <jack.douglas@rocketmail.com>
2023-07-09 21:14:41 +0100

    logic added so that users are only created on sign up, not by other users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
     has_many :posts
     has_one_attached :profile_picture
     validates :email, presence: true
-    # validates :password, presence: true
+    validates :username, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "home#index"
   devise_for :users
   resources :posts
-  resources :users
+  resources :users, only: [:show, :edit, :update, :destroy, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
