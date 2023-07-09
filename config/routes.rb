32d4f4196ebc176c1810d3bf8fff35370e7d50b0
JJD1990 <jack.douglas@rocketmail.com>
2023-07-09 21:37:48 +0100

    registrations controller added to allow username at sign up stage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :posts
   resources :users, only: [:show, :edit, :update, :destroy, :index]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-
 end
